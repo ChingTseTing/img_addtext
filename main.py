5b229524e844e80df01fb7f7f81bee5e42cf1261
ChingTseTing <81843679+ChingTseTing@users.noreply.github.com>
2022-01-11 17:23:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -164,6 +164,13 @@ def update_record(user_id, col, value, TABLE_NAME):
 
 
 
+def img_getlink( imgpath):
+	im = pyimgur.Imgur("08680019f3643c6")
+	upload_image = im.upload_image(imgpath, title="Uploaded with PyImgur")
+	return upload_image.link
+
+
+
 # 文字事件
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -172,7 +179,36 @@ def handle_message(event):
 # 照片事件   
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
-  line_bot_api.reply_message(event.reply_token, TextSendMessage(text=os.getcwd() )  )
+  SendImage = line_bot_api.get_message_content(event.message.id)
+  local_save = './Image/' + event.message.id + '.png'
+  with open(local_save, 'wb') as fd:
+    for chenk in SendImage.iter_content():
+      fd.write(chenk)
+
+  tmp = Image.open(    local_save   )
+  draw = ImageDraw.Draw(tmp)
+  font = ImageFont.truetype( font = "Times New Roman.ttf"  , size = 20 )
+  draw.text( xy=( 0.75*tmp.size[0] , 0.8*tmp.size[1] ) , text = 'Iris Chong' , fill = (255, 255 , 255) , font=font )
+  tmp.save(  local_save  )
+  
+  img_url = img_getlink(local_save)
+
+	line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=img_url, preview_image_url=img_url))
+
+
+
+  # message_content = line_bot_api.get_message_content(event.message.id)
+
+	# SendImage = line_bot_api.get_message_content(event.message.id)
+	# local_save = './static/' + event.message.id + '.png'
+	# with open(local_save, 'wb') as fd:
+	# 	for chenk in SendImage.iter_content():
+	# 		fd.write(chenk)
+
+
+  line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url = ngrok_url + "/static/" + event.message.id + ".png", preview_image_url = ngrok_url + "/static/" + event.message.id + ".png"))
+  # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=os.getcwd() )  )
+
 
 #主程式
 if __name__ == "__main__":
